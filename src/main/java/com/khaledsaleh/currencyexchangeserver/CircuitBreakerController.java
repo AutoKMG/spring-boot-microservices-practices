98ee4e1e4b5124eaa9fa6341760a7e898632226f
Khaled Saleh <2664083@tcs.com>
2023-07-20 17:48:58 +0200

    Exploring Rate Limiting and BulkHead Features of Resilience4j

--- src/main/java/com/khaledsaleh/currencyexchangeserver/CircuitBreakerController.java
+++ src/main/java/com/khaledsaleh/currencyexchangeserver/CircuitBreakerController.java
@@ -1,6 +1,8 @@
 package com.khaledsaleh.currencyexchangeserver;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +15,16 @@ public class CircuitBreakerController {
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
     @GetMapping("/sample-api")
     // @Retry(name="sample-api", fallbackMethod = "hardcodedResponse")
-    @CircuitBreaker(name="default", fallbackMethod = "hardcodedResponse")
+    // @CircuitBreaker(name="default", fallbackMethod = "hardcodedResponse")
+    // @RateLimiter(name="default")
+    @Bulkhead(name="default")
     public String sampleApi() {
         logger.info("Sample Api call received");
+        /**
         ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
         return forEntity.getBody();
+         **/
+        return "sample-api";
     }
     private String hardcodedResponse(Exception ex){
         return "fallback-response";
