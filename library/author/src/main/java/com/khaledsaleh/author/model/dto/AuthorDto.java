62e6ff5c0968ab9e340271159acb94d22b142c46
Khaled Saleh <th.be03@gmail.com>
2023-10-26 20:49:21 +0200

    Adding JsonProperty annotation to receive payload in camelcase

--- library/author/src/main/java/com/khaledsaleh/author/model/dto/AuthorDto.java
+++ library/author/src/main/java/com/khaledsaleh/author/model/dto/AuthorDto.java
@@ -1,9 +1,12 @@
 package com.khaledsaleh.author.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class AuthorDto {
+    @JsonProperty("first_name")
     private String firstName;
+    @JsonProperty("last_name")
     private String lastName;
 }
--- library/book/src/main/java/com/khaledsaleh/book/model/dto/BookDto.java
+++ library/book/src/main/java/com/khaledsaleh/book/model/dto/BookDto.java
@@ -1,10 +1,14 @@
 package com.khaledsaleh.book.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class BookDto {
+    @JsonProperty("book_name")
     private String bookName;
+    @JsonProperty("book_category")
     private String bookCategory;
+    @JsonProperty("author_id")
     private Integer authorId;
 }
